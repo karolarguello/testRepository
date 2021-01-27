b0621bd94391628f69154c50df6b565139d9bddc
karolarguello <77945949+karolarguello@users.noreply.github.com>
2021-01-26 21:05:36 -0600

    Add files via upload

---
+++ myFirstProject/src/myFirstProject/Application.java
@@ -0,0 +1,36 @@
+package myFirstProject;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int age = 28;
+double accountBalance = 234.56;
+char middleInitial = 'L';
+boolean isHotOutside = false;
+String firstName = "John";
+System.out.println(firstName);
+System.out.println(middleInitial);
+System.out.println(age);
+System.out.println(isHotOutside);
+System.out.println(accountBalance);
+
+int salary = 200;
+System.out.println(salary);
+int bonus = 100;
+System.out.println(bonus);
+boolean isSalary400 = salary == 400;
+System.out.println(isSalary400);
+
+salary = salary + bonus;
+System.out.println(salary);
+
+String mainName = "John";
+String lastName = "Lewis";
+String fullName = mainName + " " + lastName;
+System.out.println(fullName);
+
+	
+	}
+
+}
